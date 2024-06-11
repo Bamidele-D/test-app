fe9b6ff2243194ad695fa94b525de28c312886cd
bamidele-d <a.dosunmu@thekredibank.com>
2024-06-11 01:24:59 +0100

    updates

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import App from './App.vue'
 import router from './router'
 import KrediAuth from "kredi-reg-authentication";
 import "kredi-reg-authentication/styles.css";
+import 'remixicon/fonts/remixicon.css'
+import VOtpInput from "vue3-otp-input";
 
 const app = createApp(App)
 
 app.use(KrediAuth)
 app.use(router)
+app.component('v-otp-input', VOtpInput)
 
 app.mount('#app')
